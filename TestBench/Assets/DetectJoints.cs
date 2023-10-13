e703dc9345f3ce5fa58cfb9a7d7d69eb9cfb7c3b
Martin Færevaag <mkf99@live.no>
2023-10-13 11:52:40 +0200

    sdfds

---
+++ TestBench/Assets/DetectJoints.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Windows.Kinect;
+
+public class DetectJoints : MonoBehaviour
+{
+
+    public GameObject BodySrcManager;
+    public JointType TrackedJoint;
+    private BodySourceManager bodyManager;
+    private Body[] bodies;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (BodySrcManager == null)
+        {
+            Debug.Log("Assign Game Ojbect with Body Source Manager");
+        }
+        else
+        {
+            bodyManager = BodySrcManager.GetComponent<BodySourceManager>();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (BodySrcManager == null)
+        {
+            return;
+        }
+        else
+        {
+            bodies = bodyManager.GetData();
+            if (bodies == null)
+            {
+                return;
+            }
+            foreach (var body in bodies)
+            {
+                if (body == null)
+                {
+                    continue;
+                }
+                if (body.IsTracked)
+                {
+                    var pos = body.Joints[TrackedJoint].Position;
+                    gameObject.transform.position = new Vector3(pos.X, pos.Y);
+                }
+            }
+        }
+    }
+}
